251c249f9378d54699fec8cf6c26f73b5ea9dda5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-27 21:29:26 +0530

    render img

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png",
+        }}
+        style={{ width: "100%", aspectRatio: 1 }}
+      />
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
