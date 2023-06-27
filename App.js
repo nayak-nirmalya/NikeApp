7fbb51c4aff06878d09877a88f23a7701d5bd5a2
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-27 21:46:05 +0530

    added new screen component ProductsScreen

--- App.js
+++ App.js
@@ -2,19 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 
 import products from "./src/data/products";
+import ProductsScreen from "./src/screens/ProductsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-          </View>
-        )}
-        numColumns={2}
-      />
+      <ProductsScreen />
 
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
---
+++ src/screens/ProductsScreen.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View, FlatList, Image } from "react-native";
+
+import products from "../data/products";
+
+const ProductsScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+export default ProductsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+});
