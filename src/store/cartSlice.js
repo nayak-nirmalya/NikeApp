16812980ba257eb52a5a5aeb635c94986cd4f0e3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-01 11:31:45 +0530

    increase/decrease qnt in cart

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -25,6 +25,13 @@ export const cartSlice = createSlice({
         });
       }
     },
-    changeQuantity: (state, action) => {},
+    changeQuantity: (state, action) => {
+      const { productId, amount } = action.payload;
+      const cartItem = state.items.find(
+        (item) => item.product.id === productId
+      );
+
+      if (cartItem) cartItem.quantity += amount;
+    },
   },
 });
