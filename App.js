88338604f367bab69669775c3aaac97c4549f373
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-01 09:14:33 +0530

    wrapped with provider

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
 
 import Navigation from "./src/navigation";
+import { store } from "./src/store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navigation />
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Navigation />
 
-      <StatusBar style="auto" />
-    </View>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
