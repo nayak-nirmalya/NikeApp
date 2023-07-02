cd963b1207c050e772cca20c55547299664f64af
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-02 11:21:14 +0530

    added api slice

---
+++ src/store/apiSlice.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = "http://localhost:3000/";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => "products",
+    }),
+    getProduct: builder.query({
+      query: (id) => `products/${id}`,
+    }),
+    createOrder: builder.mutation({
+      query: (newOrder) => ({
+        url: "orders",
+        method: "POST",
+        body: newOrder,
+      }),
+    }),
+    getOrder: builder.query({
+      query: (ref) => `orders/${ref}`,
+    }),
+  }),
+});
+
+export const {
+  useGetProductQuery,
+  useGetProductsQuery,
+  useCreateOrderMutation,
+  useGetOrderQuery,
+} = apiSlice;
