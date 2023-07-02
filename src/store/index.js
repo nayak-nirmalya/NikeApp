d2296ea7875d5ca3b389ff8039f5089948dd3354
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-02 11:23:05 +0530

    added api and middleware to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { cartSlice } from "./cartSlice";
 import { productsSlice } from "./productsSlice";
+import { apiSlice } from "./apiSlice";
 
 export const store = configureStore({
   reducer: {
     products: productsSlice.reducer,
     cart: cartSlice.reducer,
+    api: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
