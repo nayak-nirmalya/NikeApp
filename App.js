98f94b59ab7af13fe6b12ecaa7468c9633f5e7cf
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-04 12:43:42 +0530

    read PUBLISHABLE_KEY from @env

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { Provider } from "react-redux";
 import { StripeProvider } from "@stripe/stripe-react-native";
 
+import { PUBLISHABLE_KEY } from "@env";
+
 import Navigation from "./src/navigation";
 import { store } from "./src/store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <StripeProvider>
+      <StripeProvider publishableKey={PUBLISHABLE_KEY}>
         <Navigation />
       </StripeProvider>
     </Provider>
